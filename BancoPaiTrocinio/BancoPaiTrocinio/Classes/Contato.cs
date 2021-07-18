5ea511164c810576e332577a25a1e7b8fbf4dede
Felipe-Zuck <fesilvaesilva@hotmail.com>
2021-07-18 18:35:44 -0300

    Add Classes Usuario e Contato

---
+++ BancoPaiTrocinio/BancoPaiTrocinio/Classes/Contato.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoPaiTrocinio.Classes
+{
+    class Contato
+    {
+        public int ctt_id { get; set; }
+        public int ctt_id_usuario { get; set; }
+        public string ctt_tel { get; set; }
+        public string ctt_cel { get; set; }
+        public string ctt_email { get; set; }
+
+    }
+}
---
+++ BancoPaiTrocinio/BancoPaiTrocinio/Classes/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoPaiTrocinio.Classes
+{
+    class Usuario
+    {
+        public int u_id { get; set; }
+        public string u_nome { get; set; }
+        public string u_cpf { get; set; }
+        public string u_rg { get; set; }
+        public string u_senha { get; set; }
+        public int u_id_cliente { get; set; }
+        public int u_id_func { get; set; }
+        public int u_ctt_id { get; set; }
+
+    }
+}
