7a90a2dc6bbba5aefd1607d51d1d95791121a382
Andre Sirikaku <asirikaku2000@gmail.com>
2021-07-18 18:25:01 -0300

    formularios

---
+++ BancoPaiTrocinio/BancoPaiTrocinio/CadastroCliente.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace BancoPaiTrocinio
+{
+    public partial class CadastroCliente : UserControl
+    {
+        public CadastroCliente()
+        {
+            InitializeComponent();
+            Grp_Cadastro.Text = "Cadastro";
+            Grp_DadosPessoais.Text = "Dados Pessoais";
+            Grp_Endereco.Text = "Endereço";
+            Grp_Outros.Text = "Outros";
+            Lbl_Usuario.Text = "Usuario";
+            Lbl_Senha.Text = "Senha";
+            Lbl_Senha.Text = "Confirmar Senha";
+            Lbl_Bairro.Text = "Bairro";
+            Lbl_CEP.Text = "CEP";
+            Lbl_Complemento.Text = "Complemento";
+            Lbl_CPF.Text = "CPF";
+            Lbl_Estado.Text = "Estado";
+            Lbl_Logradouro.Text = "Logradouro";
+            Lbl_NomeCliente.Text = "Nome";
+            Lbl_Profissao.Text = "Profissão";
+            Lbl_Telefone.Text = "Telefone";
+            Lbl_Cidade.Text = "Cidade";
+            Rdb_Masculino.Text = "Masculino";
+            Rdb_Feminino.Text = "Feminino";
+            Rdb_Indefinido.Text = "Indefinido";
+            Grp_Genero.Text = "Genero";
+
+            Cmb_Estados.Items.Clear();
+            Cmb_Estados.Items.Add("Acre (AC)");
+            Cmb_Estados.Items.Add("Alagoas(AL)");
+            Cmb_Estados.Items.Add("Amapá(AP)");
+            Cmb_Estados.Items.Add("Amazonas(AM)");
+            Cmb_Estados.Items.Add("Bahia(BA)");
+            Cmb_Estados.Items.Add("Ceará(CE)");
+            Cmb_Estados.Items.Add("Distrito Federal(DF)");
+            Cmb_Estados.Items.Add("Espírito Santo(ES)");
+            Cmb_Estados.Items.Add("Goiás(GO)");
+            Cmb_Estados.Items.Add("Maranhão(MA)");
+            Cmb_Estados.Items.Add("Mato Grosso(MT)");
+            Cmb_Estados.Items.Add("Mato Grosso do Sul(MS)");
+            Cmb_Estados.Items.Add("Minas Gerais(MG)");
+            Cmb_Estados.Items.Add("Pará(PA)");
+            Cmb_Estados.Items.Add("Paraíba(PB)");
+            Cmb_Estados.Items.Add("Paraná(PR)");
+            Cmb_Estados.Items.Add("Pernambuco(PE)");
+            Cmb_Estados.Items.Add("Piauí(PI)");
+            Cmb_Estados.Items.Add("Rio de Janeiro(RJ)");
+            Cmb_Estados.Items.Add("Rio Grande do Norte(RN)");
+            Cmb_Estados.Items.Add("Rio Grande do Sul(RS)");
+            Cmb_Estados.Items.Add("Rondônia(RO)");
+            Cmb_Estados.Items.Add("Roraima(RR)");
+            Cmb_Estados.Items.Add("Santa Catarina(SC)");
+            Cmb_Estados.Items.Add("São Paulo(SP)");
+            Cmb_Estados.Items.Add("Sergipe(SE)");
+            Cmb_Estados.Items.Add("Tocantins(TO)");
+
+            Tls_Principal.Items[0].ToolTipText = "Capturar um cliente já cadastrado na base";
+
+
+            LimparFormulario();
+        }
+
+        private void LimparFormulario()
+        {
+            Txt_Usuario.Text = "";
+            Txt_Senha.Text = "";
+            Txt_SenhaConfirmacao.Text = "";
+            Txt_NomeCliente.Text = "";
+            Txt_CPF.Text = "";
+            Cmb_Estados.SelectedIndex = -1;
+            Txt_CEP.Text = "";
+            Txt_Logradouro.Text = "";
+            Txt_Complemento.Text = "";
+            Txt_Cidade.Text = "";
+            Txt_Bairro.Text = "";
+            Txt_Telefone.Text = "";
+            Txt_Profissao.Text = "";
+            Rdb_Masculino.Checked = true;
+        }
+
+        private void Txt_CEP_Leave(object sender, EventArgs e)
+        {
+            string vCep = Txt_CEP.Text;
+            if (vCep != "")
+            {
+                if (vCep.Length == 8)
+                {
+                    if (Information.IsNumeric(vCep))
+                    {
+                        var vJson = Cls_Uteis.GeraJSONCEP(vCep);
+                        Cep.Unit cep = new Cep.Unit();
+                        cep = Cep.DeserializedClassUnit(vJson);
+                        Txt_Logradouro.Text = cep.logradouro;
+                        Txt_Complemento.Text = cep.complemento;
+                        Txt_Cidade.Text = cep.localidade;
+                        Txt_Bairro.Text = cep.bairro;
+                        Cmb_Estados.SelectedIndex = -1;
+                        for (int i = 0; i <= Cmb_Estados.Items.Count - 1; i++)
+                        {
+                            var vPos = Strings.InStr(Cmb_Estados.Items[i].ToString(), "(" + cep.uf + ")");
+                            if (vPos > 0)
+                            {
+                                Cmb_Estados.SelectedIndex = i;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void salvarToolStripButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ BancoPaiTrocinio/BancoPaiTrocinio/Frm_Questao.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace BancoPaiTrocinio
+{
+    partial class Frm_Questao
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_Questao));
+            this.Lbl_Questao = new System.Windows.Forms.Label();
+            this.Btn_OK = new System.Windows.Forms.Button();
+            this.Btn_Cancela = new System.Windows.Forms.Button();
+            this.Pic_Imagem = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic_Imagem)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Lbl_Questao
+            // 
+            this.Lbl_Questao.AutoSize = true;
+            this.Lbl_Questao.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_Questao.Location = new System.Drawing.Point(18, 9);
+            this.Lbl_Questao.Name = "Lbl_Questao";
+            this.Lbl_Questao.Size = new System.Drawing.Size(229, 20);
+            this.Lbl_Questao.TabIndex = 0;
+            this.Lbl_Questao.Text = "\tVocê deseja validar o CPF?";
+            // 
+            // Btn_OK
+            // 
+            this.Btn_OK.Location = new System.Drawing.Point(147, 60);
+            this.Btn_OK.Name = "Btn_OK";
+            this.Btn_OK.Size = new System.Drawing.Size(115, 23);
+            this.Btn_OK.TabIndex = 1;
+            this.Btn_OK.Text = "Sim. Continue";
+            this.Btn_OK.UseVisualStyleBackColor = true;
+            this.Btn_OK.Click += new System.EventHandler(this.Btn_OK_Click);
+            // 
+            // Btn_Cancela
+            // 
+            this.Btn_Cancela.Location = new System.Drawing.Point(147, 89);
+            this.Btn_Cancela.Name = "Btn_Cancela";
+            this.Btn_Cancela.Size = new System.Drawing.Size(115, 23);
+            this.Btn_Cancela.TabIndex = 2;
+            this.Btn_Cancela.Text = "Não. Pare";
+            this.Btn_Cancela.UseVisualStyleBackColor = true;
+            this.Btn_Cancela.Click += new System.EventHandler(this.Btn_Cancela_Click);
+            // 
+            // Pic_Imagem
+            // 
+            this.Pic_Imagem.Image = global::BancoPaiTrocinio.Properties.Resources.question;
+            this.Pic_Imagem.Location = new System.Drawing.Point(22, 43);
+            this.Pic_Imagem.Name = "Pic_Imagem";
+            this.Pic_Imagem.Size = new System.Drawing.Size(95, 79);
+            this.Pic_Imagem.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.Pic_Imagem.TabIndex = 3;
+            this.Pic_Imagem.TabStop = false;
+            // 
+            // Frm_Questao
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(289, 151);
+            this.Controls.Add(this.Pic_Imagem);
+            this.Controls.Add(this.Btn_Cancela);
+            this.Controls.Add(this.Btn_OK);
+            this.Controls.Add(this.Lbl_Questao);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Frm_Questao";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Questão";
+            ((System.ComponentModel.ISupportInitialize)(this.Pic_Imagem)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Lbl_Questao;
+        private System.Windows.Forms.Button Btn_OK;
+        private System.Windows.Forms.Button Btn_Cancela;
+        private System.Windows.Forms.PictureBox Pic_Imagem;
+    }
+}
